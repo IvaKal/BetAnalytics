7756311dc80a81d1d8eb448bb48b683431da554b
IvaKal <IvaneKaloiani@Gmail.com>
2024-03-31 17:52:13 +0400

    update

--- src/index.js
+++ src/index.js
@@ -4,43 +4,10 @@ import './index.css';
 import App from './App';
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-// class Person {
-//   greeting () {
-//     return `hi, I am ${this.firstName} ${this.lastName}.`;
-//   }
-// }
-
-// const person = new Person();
-// person.firstName = `Iva`;
-// person.lastName = `Kaloiani`;
-
-
-// console.log(person);
-
-
-// 1) კლასის განმარტებას ვერ ვხვდები, (If a car is an object, then a car factory is an object class. ანუ?)
-// 2) ზედა მაგალითში "greeting" არ აქვს არსად გამოძახებული,
-// 3) დიდი და პატარა ასო Person ში
-// 4) new რას ნიშნავს
-// 5) $ ით რატო იწყება დაბრუნება
-
-//   class Person {
-//     constructor(firstName, lastName) {
-//       this.firstName = firstName;
-//       this.lastName = lastName;
-//     }
-//     greeting() {
-//       return `Hi, I am ${this.firstName} ${this.lastName}.`;
-//     }
-//   }
-// const person = new Person('Elizabeth', 'Harmon');
-// console.log(person);
-
-// 1) ამ კონსტრუქტორსაც ვერ მივხვდი 
